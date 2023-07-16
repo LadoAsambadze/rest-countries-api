169e6b6e67ac51259ebc57189d3e98d6bec97eff
Lado Asambadze <lado.asambadze1@gmail.com>
2023-07-16 15:37:51 +0400

    feat: creat header styles for desktop resolution

--- src/App.tsx
+++ src/App.tsx
@@ -20,12 +20,14 @@ export default App;
 const Header = styled.div`
   background: #fff;
   box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.06);
-
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
   padding: 32px 16px 32px 16px;
+  @media (min-width: 1440px) {
+    padding: 24px 80px 24px 80px;
+  }
 `;
 const Question = styled.span`
   color: #111517;
@@ -33,6 +35,10 @@ const Question = styled.span`
   font-style: normal;
   font-weight: 800;
   line-height: 20px;
+  @media (min-width: 1440px) {
+    padding: 24px 80px 24px 80px;
+    font-size: 24px;
+  }
 `;
 
 const ModeDiv = styled.div`
@@ -45,6 +51,10 @@ const Moon = styled.img`
   width: 16px;
   height: 16px;
   cursor: pointer;
+  @media (min-width: 1440px) {
+    width: 20px;
+    height: 20px;
+  }
 `;
 
 const ModeSpan = styled.span`
@@ -55,4 +65,7 @@ const ModeSpan = styled.span`
   line-height: normal;
   margin-left: 8px;
   cursor: pointer;
+  @media (min-width: 1440px) {
+    font-size: 16px;
+  }
 `;
