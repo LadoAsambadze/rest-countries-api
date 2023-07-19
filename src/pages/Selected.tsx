e56e42fbae5140f48ed20826b93fad0d9db53bd1
Lado Asambadze <lado.asambadze1@gmail.com>
2023-07-19 12:35:17 +0400

    style: update styles of selected page for desktop resolution

--- src/pages/Selected.tsx
+++ src/pages/Selected.tsx
@@ -8,45 +8,57 @@ export default function Selected() {
       <Header />
       <Main>
         <InfoDiv>
-          <ButtonDiv>
-            <ArrowImg src="/back-light.svg" />
-            <Back>Back</Back>
-          </ButtonDiv>
-          <Flag src="/germany.png" />
-          <Name>Belgium</Name>
-          <InfoOne>
-            <strong>Native Name: </strong> België
-          </InfoOne>
-          <InfoOne>
-            <strong>Native Name: </strong> België
-          </InfoOne>
-          <InfoOne>
-            <strong>Native Name: </strong> België
-          </InfoOne>
-          <InfoOne>
-            <strong>Native Name: </strong> België
-          </InfoOne>
-          <InfoOne>
-            <strong>Native Name: </strong> België
-          </InfoOne>
-          <InfoOne>
-            <strong>Native Name: </strong> België
-          </InfoOne>
-          <InfoOne style={{ marginTop: "32px" }}>
-            <strong>Native Name: </strong> België
-          </InfoOne>
-          <InfoOne>
-            <strong>Native Name: </strong> België
-          </InfoOne>
-          <InfoOne>
-            <strong>Native Name: </strong> België
-          </InfoOne>
-          <Border>Border Countries: </Border>
-          <ListBox>
-            <Box>Germany</Box>
-            <Box>Germany</Box>
-            <Box>Germany</Box>
-          </ListBox>
+          <div>
+            <ButtonDiv>
+              <ArrowImg src="/back-light.svg" />
+              <Back>Back</Back>
+            </ButtonDiv>
+            <Flag src="/germany.png" />
+          </div>
+          <TextInfo>
+            <Name>Belgium</Name>
+            <About>
+              <FirstText>
+                <InfoOne>
+                  <strong>Native Name: </strong> België
+                </InfoOne>
+                <InfoOne>
+                  <strong>Native Name: </strong> België
+                </InfoOne>
+                <InfoOne>
+                  <strong>Native Name: </strong> België
+                </InfoOne>
+                <InfoOne>
+                  <strong>Native Name: </strong> België
+                </InfoOne>
+                <InfoOne>
+                  <strong>Native Name: </strong> België
+                </InfoOne>
+                <InfoOne>
+                  <strong>Native Name: </strong> België
+                </InfoOne>
+              </FirstText>
+              <SecondText>
+                <InfoOne>
+                  <strong>Native Name: </strong> België
+                </InfoOne>
+                <InfoOne>
+                  <strong>Native Name: </strong> België
+                </InfoOne>
+                <InfoOne>
+                  <strong>Native Name: </strong> België
+                </InfoOne>
+              </SecondText>
+            </About>
+            <BorderSection>
+              <Border>Border Countries: </Border>
+              <ListBox>
+                <Box>Germany</Box>
+                <Box>Germany</Box>
+                <Box>Germany</Box>
+              </ListBox>
+            </BorderSection>
+          </TextInfo>
         </InfoDiv>
       </Main>
     </>
@@ -59,6 +71,11 @@ const Main = styled.section`
   padding: 40px 28px 60px 28px;
   background: #fafafa;
   align-items: center;
+  justify-content: center;
+  @media (min-width: 1440px) {
+    padding: 80px 80px 360px 80px;
+    align-items: center;
+  }
 `;
 
 const ButtonDiv = styled.div`
@@ -93,11 +110,18 @@ const Flag = styled.img`
   height: 275.924px;
   width: 319.837px;
   border-radius: 5px;
+  @media (min-width: 1440px) {
+    width: 560px;
+    height: 401px;
+  }
 `;
 
 const InfoDiv = styled.div`
   display: flex;
   flex-direction: column;
+  @media (min-width: 1440px) {
+    flex-direction: row;
+  }
 `;
 
 const Name = styled.h1`
@@ -108,6 +132,10 @@ const Name = styled.h1`
   line-height: normal;
   margin-top: 28px;
   margin-bottom: 20px;
+  @media (min-width: 1440px) {
+    font-size: 32px;
+    margin-bottom: 25px;
+  }
 `;
 
 const InfoOne = styled.span`
@@ -116,6 +144,19 @@ const InfoOne = styled.span`
   font-style: normal;
   font-weight: 600;
   line-height: 32px;
+  @media (min-width: 1440px) {
+    font-size: 16px;
+    font-weight: 300;
+  }
+`;
+const BorderSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  @media (min-width: 1440px) {
+    flex-direction: row;
+    align-items: center;
+    margin-top: 70px;
+  }
 `;
 const Border = styled.h1`
   color: #111517;
@@ -124,6 +165,9 @@ const Border = styled.h1`
   font-weight: 600;
   line-height: 24px;
   margin-top: 34px;
+  @media (min-width: 1440px) {
+    margin-top: 0;
+  }
 `;
 
 const ListBox = styled.div`
@@ -132,10 +176,50 @@ const ListBox = styled.div`
   justify-content: space-between;
   align-items: center;
   margin-top: 16px;
+  @media (min-width: 1440px) {
+    margin-top: 0;
+    margin-left: 16px;
+  }
 `;
 const Box = styled.div`
   padding: 6px 16px 6px 16px;
   border-radius: 2px;
   background: #fff;
   box-shadow: 0px 0px 4px 1px rgba(0, 0, 0, 0.1);
+  @media (min-width: 1440px) {
+    margin-left: 16px;
+  }
+`;
+
+const TextInfo = styled.div`
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  @media (min-width: 1440px) {
+    margin-left: 120px;
+    margin-top: 80px;
+  }
+`;
+
+const About = styled.div`
+  display: flex;
+  flex-direction: column;
+  @media (min-width: 1440px) {
+    flex-direction: row;
+  }
+`;
+
+const FirstText = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const SecondText = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 32px;
+  @media (min-width: 1440px) {
+    margin-left: 141px;
+    margin-top: 0;
+  }
 `;
